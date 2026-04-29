86948a590887652ba0ae8d20a252059b62a02688
longvotheengineer <longdaicangu0002@gmail.com>
2026-04-29 08:38:03 +0700

    [clean]: Delete garbage files (#77)

--- Software/Middleware/robot_ros2/src/paper/_backups/backup_2026-04-26_pid_balance_plots/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple script to compile the LaTeX paper
-
-echo "Compiling LaTeX document..."
-pdflatex -interaction=nonstopmode main.tex
-bibtex main
-pdflatex -interaction=nonstopmode main.tex
-pdflatex -interaction=nonstopmode main.tex
-echo "Done! Check main.pdf"
