0cc97f5d4230d9c9906f89d1854b7640e8e4f6f6
longvotheengineer <longdaicangu0002@gmail.com>
2026-04-29 14:05:53 +0700

    Adjust the foot height - 0 -5 0 -10 (#77)

--- Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
+++ Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
@@ -149,7 +149,7 @@ class GaitConfig:
     LF_Z_OFFSET = 0
     LB_Z_OFFSET = -5
     RF_Z_OFFSET = 0
-    RB_Z_OFFSET = -9
+    RB_Z_OFFSET = -10
 
     PARAMS_GAIT_FORWARD = {
         "left-front":    {"x_center":  125, "y_val":  135, "reverse": False, "z_offset": LF_Z_OFFSET},
@@ -176,16 +176,16 @@ class GaitConfig:
 
     PARAMS_GAIT_TURN_RIGHT = {
         "left-front":    {"x_center":  125, "y_val":  135, "reverse": False, "z_offset": LF_Z_OFFSET},
-        "left-behind":   {"x_center": -100, "y_val":  135, "reverse": False, "z_offset": LB_Z_OFFSET},
+        "left-behind":   {"x_center": -60, "y_val":  135, "reverse": False, "z_offset": LB_Z_OFFSET},
         "right-front":   {"x_center":  125, "y_val": -135, "reverse": True, "z_offset": RF_Z_OFFSET},
-        "right-behind":  {"x_center": -100, "y_val": -135, "reverse": True, "z_offset": RB_Z_OFFSET},
+        "right-behind":  {"x_center": -60, "y_val": -135, "reverse": True, "z_offset": RB_Z_OFFSET},
     }
 
     PARAMS_GAIT_TURN_LEFT = {
         "left-front":    {"x_center":  125, "y_val":  135, "reverse": True, "z_offset": LF_Z_OFFSET},
-        "left-behind":   {"x_center": -100, "y_val":  135, "reverse": True, "z_offset": LB_Z_OFFSET},
+        "left-behind":   {"x_center": -110, "y_val":  135, "reverse": True, "z_offset": LB_Z_OFFSET},
         "right-front":   {"x_center":  125, "y_val": -135, "reverse": False, "z_offset": RF_Z_OFFSET},
-        "right-behind":  {"x_center": -100, "y_val": -135, "reverse": False, "z_offset": RB_Z_OFFSET},
+        "right-behind":  {"x_center": -110, "y_val": -135, "reverse": False, "z_offset": RB_Z_OFFSET},
     }
 
     PARAMS_PHASESHIFT_TROT = {
