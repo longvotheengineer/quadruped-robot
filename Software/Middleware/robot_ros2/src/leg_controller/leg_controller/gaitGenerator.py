e2557b2c51f184f16ae15fc88b001b600718ea67
longvotheengineer <longdaicangu0002@gmail.com>
2026-05-05 21:15:22 +0700

    Update params

--- Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
+++ Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
@@ -266,7 +266,7 @@ class Gait:
         robot_length = RobotLength(L=209, W=191, l1=26, l2=106, l3=125)
         self._kinematics = Kinematics(self._node, robot_length, use_real=node.use_real)
         self.serial_publish = SerialPublish(self._node)
-        self._waypoint = Waypoint(200, 160, 60, 1000)
+        self._waypoint = Waypoint(200, 200, 55, 1000)
 
         # Trajectory state
         self._angle_data = None
