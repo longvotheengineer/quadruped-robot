fd6b7fe0284393ad4404609f727d1e1fbf675c8d
longvotheengineer <longdaicangu0002@gmail.com>
2026-05-06 20:19:01 +0700

    Update params

--- Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
+++ Software/Middleware/robot_ros2/src/leg_controller/leg_controller/gaitGenerator.py
@@ -266,7 +266,7 @@ class Gait:
         robot_length = RobotLength(L=209, W=191, l1=26, l2=106, l3=125)
         self._kinematics = Kinematics(self._node, robot_length, use_real=node.use_real)
         self.serial_publish = SerialPublish(self._node)
-        self._waypoint = Waypoint(200, 200, 30, 1000)
+        self._waypoint = Waypoint(200, 200, 45, 1000)
 
         # Trajectory state
         self._angle_data = None
@@ -563,13 +563,13 @@ class Gait:
 
     def _trajectory_moving(self, x_center, y_val, reverse=False, z_offset=0):
         """Build D-shape foot path in Cartesian space (x, y, z)."""
-        stride_length = 20
+        stride_length = 10
         x_forward = x_center + stride_length / 2
         x_backward = x_center - stride_length / 2
         z_stance = -170 + z_offset
         z_swing = -130
         lift_height = z_swing - z_stance
-        lift_height = 100
+        lift_height = 80
 
         if reverse:
             pos_A = [x_forward, y_val, z_stance]
